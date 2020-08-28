34668776e4eb992b6f5b5de2d6c07a45fa912788
Khashayar <khashayar.abryoghani@rwth-aachen.de>
2020-08-28 21:41:42 +0200

    UI Improvements in AskingView

--- Daily Portfolio Update/Views/AskingView.swift
+++ Daily Portfolio Update/Views/AskingView.swift
@@ -16,7 +16,7 @@ struct AskingView: View {
     @State private var amountOfStock = ""
     @State private var manualPurchasedPrice = ""
     @State private var today = get_today()
-        
+    
     // this variable is somit shared through all views with this line
     @EnvironmentObject var settings: UserSettings
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
@@ -44,7 +44,7 @@ struct AskingView: View {
                         DatePicker("Your stock purchase date:", selection: $selectedDate, displayedComponents: .date)
                         
                         TextField("Purchased amount", text: $amountOfStock)
-                            //                            .keyboardType(.numberPad)
+                            .keyboardType(.decimalPad)
                             .onReceive(Just(amountOfStock)) { newValue in
                                 let filtered = newValue.filter { ".0123456789".contains($0) }
                                 if filtered != newValue {
@@ -56,12 +56,12 @@ struct AskingView: View {
                 Toggle(isOn: $toggleOn,
                        label: {
                         Text("Enter price per share manually")
-                        .lineLimit(1)
+                            .lineLimit(1)
                             .minimumScaleFactor(0.1)
                 })
                 if toggleOn{
                     TextField("Price per share", text: $manualPurchasedPrice)
-                        //                        .keyboardType(.numberPad)
+                        .keyboardType(.decimalPad)
                         .onReceive(Just(manualPurchasedPrice)) { newValue in
                             let filtered = newValue.filter { ".0123456789".contains($0) }
                             if filtered != newValue {
@@ -69,52 +69,49 @@ struct AskingView: View {
                             }
                     }
                 }
+                
+                Section(footer: Text("If you don't set the price manually the purchased price will be the close price of the purchase date.")){
+                    Button(action: {
+                        isLoading = true
+                        let df = DateFormatter()
+                        df.dateFormat = "yyyy-MM-dd"
+                        let date = df.string(from: self.selectedDate)
+                        
+                        let userInput: UserInput
+                        if self.toggleOn && self.manualPurchasedPrice != ""{
+                            userInput = UserInput(compName: self.compName, purchaseDate:date, purchaseAmount: Double(self.amountOfStock)!, manualPurchasedPrice: Double(self.manualPurchasedPrice)!)
+                        }
+                        else{
+                            userInput = UserInput(compName: self.compName, purchaseDate:date, purchaseAmount: Double(self.amountOfStock)!)
+                        }
+                        
+                        self.settings.portfolio.append(userInput)
+                        print(self.selectedDate)
+                        
+                        self.presentationMode.wrappedValue.dismiss()
+                        self.presentationMode.wrappedValue.dismiss()
+                        
+                    }) {
+                        HStack {
+                            Image(systemName: "cart.badge.plus")
+                                .font(.title)
+                            Text("Done")
+                                .fontWeight(.semibold)
+                                .font(.callout)
+                            
+                        }
+                        .frame(minWidth: 0, maxWidth: 200)
+                        .padding(.vertical,15)
+                        .foregroundColor(buttonTextColor)
+                        .cornerRadius(40)
+                        
+                    }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+                    .disabled(buttonTextColor == .gray)
                 }
+            }
+                
             .gesture(DragGesture().onChanged{_ in UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)})
-                
-            .padding(.bottom, -1111.0)
-            
-            //
-            Spacer()
-            Button(action: {
-                isLoading = true
-                let df = DateFormatter()
-                df.dateFormat = "yyyy-MM-dd"
-                let date = df.string(from: self.selectedDate)
-                
-                let userInput: UserInput
-                if self.toggleOn && self.manualPurchasedPrice != ""{
-                    userInput = UserInput(compName: self.compName, purchaseDate:date, purchaseAmount: Double(self.amountOfStock)!, manualPurchasedPrice: Double(self.manualPurchasedPrice)!)
-                }
-                else{
-                    userInput = UserInput(compName: self.compName, purchaseDate:date, purchaseAmount: Double(self.amountOfStock)!)
-                }
-                
-                self.settings.portfolio.append(userInput)
-                print(self.selectedDate)
-                
-                self.presentationMode.wrappedValue.dismiss()
-                self.presentationMode.wrappedValue.dismiss()
-                
-            }) {
-                HStack {
-                    Image(systemName: "cart.badge.plus")
-                        .font(.title)
-                    Text("Done")
-                        .fontWeight(.semibold)
-                        .font(.callout)
-                    
-                }
-                .frame(minWidth: 0, maxWidth: 200)
-                .padding()
-                .foregroundColor(buttonTextColor)
-                .background(LinearGradient(gradient: Gradient(colors: [Color.red, Color.blue]), startPoint: .leading, endPoint: .trailing))
-                .cornerRadius(40)
-                .padding(.vertical, 30)
-                
-            }.frame(alignment: .bottom)
-                .disabled(buttonTextColor == .gray)
-            //
         }
     }
     
@@ -122,10 +119,10 @@ struct AskingView: View {
         let df = DateFormatter()
         df.dateFormat = "yyyy-MM-dd"
         let date = df.string(from: self.selectedDate)
-        let color: Color = (amountOfStock != "") && (today>date) && (!toggleOn || manualPurchasedPrice != "") ? .white : .gray
+        let color: Color = (amountOfStock != "") && (today>date) && (!toggleOn || manualPurchasedPrice != "") ? .blue : .gray
         return color
     }
-
+    
 }
 
 // to use this first comment out @Binding var shouldPopToRootView : Bool
@@ -134,5 +131,3 @@ struct AskingView_Previews: PreviewProvider {
         AskingView(compName: "Apple Inc.")
     }
 }
-
-
