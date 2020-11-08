b0e1d3e6cb5f91aaeab3a8424918e025c0de0237
Khashayar <khashayar.abryoghani@rwth-aachen.de>
2020-11-09 00:56:53 +0100

    Now with comp logo in MoreInfo View

--- PortfolioPedia/Views/MoreInfo.swift
+++ PortfolioPedia/Views/MoreInfo.swift
@@ -11,30 +11,44 @@ import SwiftUI
 struct MoreInfo: View {
     @State var dataEntries: [CompPortfolioOutput]
     @Environment(\.presentationMode) private var presentationMode
+    @EnvironmentObject var settings: UserSettings
     
     var body: some View {
         VStack() {
             HStack() {
-                Text("Name:")
-                    .font(.headline)
-                    .padding(.leading, 20)
-                Text(dataEntries[0].compName)
-                Spacer()
+                if settings.showLogos{
+                    Image(myDic_Symb2Img[dataEntries[0].compSymbol]!)
+                        .resizable()
+                        .aspectRatio(1, contentMode: .fit)
+                        .cornerRadius(10)
+                        .padding(.leading)
+                        .padding(.leading)
+                }
+                VStack() {
+                    HStack() {
+                        Text("Name:")
+                            .font(.headline)
+                            .padding(.leading, 10)
+                        Text(dataEntries[0].compName)
+                        Spacer()
+                    }
+                    HStack() {
+                        Text("Market Symbol:")
+                            .font(.headline)
+                            .padding(.leading, 10)
+                        Text(dataEntries[0].compSymbol)
+                        Spacer()
+                    }
+                }
             }
-            .padding(.top)
-            HStack() {
-                Text("Market Symbol:")
-                    .font(.headline)
-                    .padding(.leading, 20)
-                Text(dataEntries[0].compSymbol)
-                Spacer()
-            }
+            .frame(height: 45)
+            .padding(.top, 9)
             Spacer()
             InfoSheet(dataEntries: dataEntries)
         }
         .minimumScaleFactor(0.1)
         .lineLimit(1)
-            
+        
         .navigationBarTitle("Overview", displayMode: .inline)        
     }
 }
